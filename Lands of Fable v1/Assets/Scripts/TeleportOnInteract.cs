d2320d3913508da33aecf555467f2b8a27896224
Thomas <thomasviard@orange.fr>
2025-01-24 14:21:33 +0100

    terminally ill commit

---
+++ Lands of Fable v1/Assets/Scripts/TeleportOnInteract.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class TeleportOninteract : MonoBehaviour
+{
+    [SerializeField] private Transform receivingPortal;
+    private bool isTeleporting = false;
+}
